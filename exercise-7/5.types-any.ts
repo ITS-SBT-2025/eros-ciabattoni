afb32adfa5fb7553317692de1ea9ca1aa8abad54
eros-ciabattoni-inim <eros.ciabattoni@inim.it>
2025-06-20 12:19:24 +0200

    refactor: reorder code examples for clarity in type handling

--- exercise-7/5.types-any.ts
+++ exercise-7/5.types-any.ts
@@ -1,9 +1,4 @@
-// Caso 1: ho una funzione che ritorna il tipo any
-const json = '{"x": 10, "y": 20}';
-const coordinates = JSON.parse(json);
-console.log(coordinates);
-
-// Caso 2: variabile dichiarata e inizializzata successivamente
+// Caso 1: variabile dichiarata e inizializzata successivamente
 let words = ["red", "green", "blue"];
 let foundWord;
 
@@ -12,3 +7,8 @@ for (const word of words) {
     foundWord = true;
   }
 }
+
+// Caso 2: ho una funzione che ritorna il tipo any
+const json = '{"x": 10, "y": 20}';
+const coordinates = JSON.parse(json);
+console.log(coordinates);
